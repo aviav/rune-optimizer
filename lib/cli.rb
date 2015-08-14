2d0eff8769fc75d401e00f2c960d8e5b5a899613
aviav <8aviav@gmail.com>
2015-08-14 12:23:58 +0200

    Enable setting scrape and page_number from CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,9 +3,10 @@ require_relative 'rune-optimizer.rb'
 
 class RuneCLI < Thor
   desc 'optimize', 'optimize runes'
-  option :scrape, type: :boolean, default: :false
-  def optimize(scrape: :false)
-    run_optimizer(scrape)
+  option :scrape, type: :boolean
+  option :page_number, type: :numeric, default: 2
+  def optimize
+    run_optimizer(options[:scrape], options[:page_number])
   end
 end
 
